786d962b69b822410d5317555334cf8ff6d59e25
PiusKariuki <piuskariuki19@gmail.com>
2023-10-01 13:11:04 +0300

    try catch

--- src/modules/visits/views/Overweight.vue
+++ src/modules/visits/views/Overweight.vue
@@ -17,22 +17,30 @@ const router = useRouter()
 const route = useRoute()
 
 const registerPatient = async (evt) => {
-  evt.preventDefault()
-  console.log('hit')
-  console.log('is valid', registrationStore.isOverweightFormValid)
-  state.loading = true
-  const {status, } = await useFetch(`/patients/${route.params.patientId}`, {method: "POST", data: {...registrationStore}})
-  console.log('status', status.value)
-
-  state.loading = false
-  if (status.value === 201) {
-    Swal.fire({
-      icon: "success",
-      timer: 1000
+  try {
+    evt.preventDefault()
+    console.log('hit')
+    console.log('is valid', registrationStore.isOverweightFormValid)
+    state.loading = true
+    const {status} = await useFetch(`/patients/${route.params.patientId}`, {
+      method: "POST",
+      data: {...registrationStore}
     })
-    await registrationStore.$reset()
-    await router.push("/")
+    console.log('status', status.value)
+
+    state.loading = false
+    if (status.value === 201) {
+      Swal.fire({
+        icon: "success",
+        timer: 1000
+      })
+      await registrationStore.$reset()
+      await router.push("/")
+    }
+  } catch (e) {
+    console.log('e', e)
   }
+
 
 }
 
